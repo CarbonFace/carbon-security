bdd5599e1bbd5af594a880914300ebac3cc41c2c
CarbonFace <553127022@qq.com>
2021-03-31 17:51:49 +0800

    security module of the CarbonFace Cloud
    2021-3-31 17:51:36  Xiaoyi Ye

---
+++ src/main/java/cn/carbonface/carbonsecurity/CarbonSecurityApplication.java
@@ -0,0 +1,13 @@
+package cn.carbonface.carbonsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarbonSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/carbonface/carbonsecurity/CarbonSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.carbonface.carbonsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
